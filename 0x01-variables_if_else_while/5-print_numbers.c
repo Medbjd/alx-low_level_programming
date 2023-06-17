1f92b6136860f1868a9eb0ce64cfe023a8f5dfc5
Medbjd <mohamedboujida51@gmail.com>
2023-06-16 18:18:52 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,16 @@
  */
 int main(void)
 {
-	int number = 0;
-
-	while (number <= 9)
-	{
-		putchar(number + '0');
-		number++;
-	}
+	putchar('0' + 0);
+	putchar('0' + 1);
+	putchar('0' + 2);
+	putchar('0' + 3);
+	putchar('0' + 4);
+	putchar('0' + 5);
+	putchar('0' + 6);
+	putchar('0' + 7);
+	putchar('0' + 8);
+	putchar('0' + 9);
 
 	putchar('\n');
 
