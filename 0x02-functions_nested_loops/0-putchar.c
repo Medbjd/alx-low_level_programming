f55fdac56f09815f56606eb3743ac6ca53025260
Medbjd <mohamedboujida51@gmail.com>
2023-06-21 17:24:08 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,20 @@
 
 /**
  * main - Entry point
- * return: Always 0
+ * Return: Always 0
  */
 
-int main(void)
+int main() 
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return 0;
 }
