6805bcb778a47fd4b170ce7dc7b4b27b683e517c
Medbjd <mohamedboujida51@gmail.com>
2023-07-04 03:09:40 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - Entry point
+ * _memset - fills memory with a constant byte
  * @s: pointed destination
  * @b: constant byte
  * @n: bytes
- * return: Always 0 (Success)
+ * return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
+		*(s + i) =  b;
+
 	return (s);
 }
